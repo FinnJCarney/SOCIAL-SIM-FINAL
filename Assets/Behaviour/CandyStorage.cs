ae65925858503eea48d5f13a1f59385bbe325f42
Finn <finnjcarney@gmail.com>
2019-12-08 21:38:58 +0000

    Initial Commit

---
+++ Assets/Behaviour/CandyStorage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyStorage : MonoBehaviour
+{
+
+    public string Name;
+    public int Amount;
+    public float Price;
+
+    public TextMesh text;
+
+    public TextMesh tm;
+    public TextMesh PriceText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = GetComponentInChildren<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tm.text = "" + Amount;
+        PriceText.text = "" + Price;
+    }
+}
---
+++ Assets/ChangePrice.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePrice : MonoBehaviour
+{
+
+    public bool IncreasePrice;
+
+    public CandyStorage candy;
+
+    public float priceChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void OnMouseDown()
+    {
+        if(IncreasePrice)
+        {
+            candy.Price += priceChange;
+        }
+        else
+        {
+            candy.Price -= priceChange;
+        }
+    }
+}
